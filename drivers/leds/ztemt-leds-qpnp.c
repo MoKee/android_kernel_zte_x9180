e9eafa84f60b8dc8d64c4fe9f9ce6fc7ee072825
Mao Li <maol@codeaurora.org>
2014-07-30 10:00:34 +0800

    leds: leds-qpnp: allocate ordered work queue for led
    From user space, the call procedures of red led blink are as
    below:
    1. turn off red led.
    2. blink red led.
    
    Each above step will be transitioned from user space to kernel
    driver and trigger a led WORK. The order from user space is
    very important because if the step 2 completes before step 1,
    then the red led will be turned off, while the user wants to
    blink it.
    
    On kernel version 3.4, below sequences will cause the order
    from user space fail:
    1. CPU0 schedule a led WORK on system_wq, which is to turn off
       the red led.
    2. CPU1 schedule a led WORK on system_wq, which is to blink the
       red led.
    3. Although the first WORK is queued before the second WORK,
       both of them can executed concurrently on CPU0 and CPU1.
    4. CPU0's workload is very heavy because it will handle almost
       all the hardware interrupt, so it is probably that the first
       WORK thread is scheduled out for some time. At that moment,
       the second WORK can complete faster than the first WORK.
       This finally cause the red led is first blinking then been
       turned off.
    
    To solve this issue on Kernel version 3.4, we can create an
    ordered workqueue which will promise us that the same led WORK
    will not be scheduled on different cpu and cannot be executed on
    different cpu concurrently.
    
    On kernel version 3.10, because the default system_wq has
    already promised the concurrency of the same WORK, so we don't
    need to use ordered workqueue for led module.
    
    Change-Id: I23fda20f2951bfcebb7ce7c9ecea542435496efe
    CRs-Fixed: 714466 703170
    Signed-off-by: Mao Li <maol@codeaurora.org>
    Signed-off-by: Abinaya P <abinayap@codeaurora.org>

--- drivers/leds/ztemt-leds-qpnp.c
+++ drivers/leds/ztemt-leds-qpnp.c
@@ -516,6 +516,7 @@ struct rgb_config_data {
  * struct qpnp_led_data - internal led data structure
  * @led_classdev - led class device
  * @delayed_work - delayed work for turning off the LED
+ * @workqueue - dedicated workqueue to handle concurrency
  * @work - workqueue for led
  * @id - led index
  * @base_reg - base register given in device tree
@@ -530,6 +531,7 @@ struct qpnp_led_data {
 	struct led_classdev	cdev;
 	struct spmi_device	*spmi_dev;
 	struct delayed_work	dwork;
+	struct workqueue_struct *workqueue;
 	struct work_struct	work;
 	int			id;
 	u16			base;
@@ -543,6 +545,7 @@ struct qpnp_led_data {
 	struct mpp_config_data	*mpp_cfg;
 	int			max_current;
 	bool			default_on;
+	bool                    in_order_command_processing;
 	int			turn_off_delay_ms;
 };
 
@@ -1506,7 +1509,10 @@ static void qpnp_led_set(struct led_classdev *led_cdev,
 		value = led->cdev.max_brightness;
 
 	led->cdev.brightness = value;
-	schedule_work(&led->work);
+	if (led->in_order_command_processing)
+		queue_work(led->workqueue, &led->work);
+	else
+		schedule_work(&led->work);
 }
 
 static void __qpnp_led_work(struct qpnp_led_data *led,
@@ -2300,6 +2306,7 @@ static void led_blink(struct qpnp_led_data *led,
 {
 	int rc;
 
+	flush_work(&led->work);
 	mutex_lock(&led->lock);
 	if (pwm_cfg->use_blink) {
 		if (led->cdev.brightness) {
@@ -3924,6 +3931,24 @@ static int __devinit qpnp_leds_probe(struct spmi_device *spmi)
 		}
 
 		mutex_init(&led->lock);
+
+		led->in_order_command_processing = of_property_read_bool
+				(temp, "qcom,in-order-command-processing");
+
+		if (led->in_order_command_processing) {
+			/*
+			 * the command order from user space needs to be
+			 * maintained use ordered workqueue to prevent
+			 * concurrency
+			 */
+			led->workqueue = alloc_ordered_workqueue
+							("led_workqueue", 0);
+			if (!led->workqueue) {
+				rc = -ENOMEM;
+				goto fail_id_check;
+			}
+		}
+
 		INIT_WORK(&led->work, qpnp_led_work);
 
 		rc =  qpnp_led_initialize(led);
@@ -4019,6 +4044,8 @@ static int __devinit qpnp_leds_probe(struct spmi_device *spmi)
 fail_id_check:
 	for (i = 0; i < parsed_leds; i++) {
 		mutex_destroy(&led_array[i].lock);
+		if (led_array[i].in_order_command_processing)
+			destroy_workqueue(led_array[i].workqueue);
 		led_classdev_unregister(&led_array[i].cdev);
 	}
 
@@ -4033,6 +4060,8 @@ static int __devexit qpnp_leds_remove(struct spmi_device *spmi)
 	for (i = 0; i < parsed_leds; i++) {
 		cancel_work_sync(&led_array[i].work);
 		mutex_destroy(&led_array[i].lock);
+		if (led_array[i].in_order_command_processing)
+			destroy_workqueue(led_array[i].workqueue);
 		led_classdev_unregister(&led_array[i].cdev);
 		switch (led_array[i].id) {
 		case QPNP_ID_WLED:
